7bffd2efcd593c91ca45dc35e9a97a25cd33a540
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2021-11-05 16:56:43 +0530

    added index

--- A9-Linux/200010003/script1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Shell script to sort the file /etc/passwd in ascending order based on the 3rd column entries, as numbers
-sort /etc/passwd -nt ":" -k3
-
--- A9-Linux/200010003/script2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Program to cut usernames from the file /etc/passwd
-cut /etc/passwd -d ":" -f1

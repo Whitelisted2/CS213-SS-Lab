a4358d04c9c7fde4515f87df1bb1572b6c5d9000
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2021-11-08 23:59:12 +0530

    Update album.php

--- A5-PHP/album.php
+++ A5-PHP/album.php
@@ -110,5 +110,57 @@
   				<br><br>
 			</div>
 		</form>
+		<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+function openModal()
+{
+	document.getElementById("myModal").style.display = "block";
+}
+  
+function closeModal()
+{
+    document.getElementById("myModal").style.display = "none";
+}
+  
+// for the navigation controls
+function slideShift(n)
+{
+	showSlides(slideIndex += n);
+}
+
+// for slide that is being shown
+function currentSlide(n)
+{
+    showSlides(slideIndex = n);
+}
+  
+function showSlides(n)
+{
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length)
+    {
+		slideIndex = 1; // move back to beginning
+	}
+	if (n < 1)
+    {
+		slideIndex = slides.length; // go to that position
+	}
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active"; 
+    captionText.innerHTML = dots[slideIndex-1].alt; // use caption as alt text as well
+}
+		</script>
 	</body>
 </html>
